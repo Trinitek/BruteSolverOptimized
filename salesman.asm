951d021db738e332555f39f89c31808345f9057c
Trinitek <trinitek@live.com>
2015-09-20 03:41:14 -0400

    Remove old algorithm, add reg push/pop macros

--- salesman.asm
+++ salesman.asm
@@ -4,42 +4,90 @@ entry DllMain
 
 include 'include/win64a.inc'
 
+define array            rcx
+define arrayLength      r9
+define distances        r8
+define mulv             r9
+define limit            r10
+define eA               rbx
+define a                r11
+define z                r12
+define z2               r13
+define p_array          r14
+define i                r15
+define v                xmm1
 define shortestDistance xmm0
 
+macro save_volatile {
+    push rax
+    push rcx
+    push rdx
+    push r8
+    push r9
+    push r10
+    push r11
+    movsd xmm6, xmm0
+    movsd xmm7, xmm1
+}
+
+macro restore_volatile {
+    movsd xmm1, xmm7
+    movsd xmm0, xmm6
+    pop r11
+    pop r10
+    pop r9
+    pop r8
+    pop rdx
+    pop rcx
+    pop rax
+}
+
+macro save_nonvolatile {
+    push rbx
+    push r12
+    push r13
+    push r14
+    push r15
+    sub rsp, 8
+    movlpd [rsp], xmm6
+    sub rsp, 8
+    movlpd [rsp], xmm7
+}
+
+macro restore_nonvolatile {
+    movlpd xmm7, [rsp]
+    add rsp, 8
+    movlpd xmm6, [rsp]
+    add rsp, 8
+    pop r15
+    pop r14
+    pop r13
+    pop r12
+    pop rbx
+}
+
 ; --> All macro parameters are registers, not constants or pointers.
-; --> rax, rbx, and rdx are volatile.
+; --> rax and rdx are volatile.
 ; --> Expects (double)0.0 to be defined in xmm7.
-; void handle(
-;   int array[], (PTR)
-;   int arrayLength,
-;   double distances2[], (PTR)
-;   int limit,
-;   int eA,
-;   int mulv)
-; TEMPS:
-;   double v
-;   int a
-;   int z
-;   int z2
-macro handle array*, arrayLength*, distances2*, limit*, eA*, mulv*, v*, a*, z* {
+macro handle {
     local return
     
-    ; v += distances2[(a * mul) + (a = array[z + 1])]
+    ; v += distances[(a * mul) + (a = array[z + 1])]
     macro addv1 \{
         mov rax, a
         mul mulv                ; rax = a * mulv
         
-        mov rbx, z2
-        shl rbx, 3
-        add rbx, array          ; rbx = &array[z + 1]
+        mov rdx, z2
+        shl rdx, 3
+        add rdx, array          ; rdx = &array[z + 1]
         
-        mov a, [rbx]            ; a = array[z + 1]
+        mov a, [rdx]            ; a = array[z + 1]
         
         add rax, a              ; rax = (a * mul) + (a = array[z + 1])
         shl rax, 3
-        add rax, distances2     ; rax = &distances2[rax]
+        add rax, distances      ; rax = &distances[rax]
         
-        addsd v, [rax]          ; v += distances2[rax]
+        addsd v, [rax]          ; v += distances[rax]
     \}
     
     ; double v = 0
@@ -58,7 +106,7 @@ macro handle array*, arrayLength*, distances2*, limit*, eA*, mulv*, v*, a*, z* {
         cmp z, 5
         jae for_1_end
         
-        ; v += distances2[(a * mul) + (a = array[z + 1])]
+        ; v += distances[(a * mul) + (a = array[z + 1])]
         addv1
         
         inc z
@@ -75,11 +123,11 @@ macro handle array*, arrayLength*, distances2*, limit*, eA*, mulv*, v*, a*, z* {
         cmp z, limit
         jae for_2_end
         
-        ; if ((v += distances2[(a * mul) + (a = array[z + 1])]) > shortestDistance)
+        ; if ((v += distances[(a * mul) + (a = array[z + 1])]) > shortestDistance)
         local if_1
         local if_1_end
         if_1:
-            ; v += distances2[(a * mul) + (a = array[z + 1])]
+            ; v += distances[(a * mul) + (a = array[z + 1])]
             addv1
             
             comisd shortestDistance, v
@@ -91,26 +139,26 @@ macro handle array*, arrayLength*, distances2*, limit*, eA*, mulv*, v*, a*, z* {
         
         for_2_end:
 
-    ; if ((v += distances2[eA + array[0]]) < shortestDistance)
+    ; if ((v += distances[eA + array[0]]) < shortestDistance)
     local if_2
     local if_2_end
     if_2:
-        ; v += distances2[eA + array[0]]
+        ; v += distances[eA + array[0]]
         mov rax, eA
         add rax, [array]
         shl rax, 3
-        add rax, distances2
+        add rax, distances
         addsd v, [rax]
     
         comisd v, shortestDistance
                                 ; if v < shortestDistance then set CF
         jnc if_2_end
     
-        ; if ((v += distances2[eA + array[limit]]) < shortestDistance)
+        ; if ((v += distances[eA + array[limit]]) < shortestDistance)
         local if_3
         local if_3_end
         if_3:
-            ; v += distances2[eA + array[limit]]
+            ; v += distances[eA + array[limit]]
             mov rax, limit
             shl rax, 3
             add rax, array
@@ -118,7 +166,7 @@ macro handle array*, arrayLength*, distances2*, limit*, eA*, mulv*, v*, a*, z* {
             
             add rax, eA
             shl rax, 3
-            add rax, distances2
+            add rax, distances
             addsd v, [rax]
         
             comisd v, shortestDistance
@@ -148,242 +196,55 @@ proc testcall
     ret
 endp
 
-; double permute(uint64_t* array, uint64_t arrayLength, double** distances, uint64_t limit)
-proc permute array, arrayLength, distances, limit, h_heap, p
-
-    ; Save parameters to stack
-    mov [array], rcx
-    dec rdx                     ; arrayLength--
-    mov [arrayLength], rdx      ; In Java, array.length represents (numberOfElements - 1)
-    mov [distances], r8
-    mov [limit], r9
+; double permute(uint64_t* array, uint64_t arrayLength, double* distances)
+proc permute h_heap, p
     
-    ; double r = 0.0
-    movlpd xmm7, [const.zero]
-    movsd xmm1, xmm7
+    ; Save nonvolatile registers, as required by the calling convention
+    save_nonvolatile
+    
+    ; Initialize registers
+    mov arrayLength, rdx        ; Move parameter out of volatile register
+    dec arrayLength             ; In Java, array.length represents (numberOfElements - 1)
+    mov mulv, rdx               ; mulv = arrayLength + 1
+    mov limit, arrayLength
+    dec limit                   ; limit = arrayLength - 1
+    mov rax, arrayLength
+    mul mulv
+    mov eA, rax                 ; eA = arrayLength * mulv
+    
+    movlpd xmm7, [const.zero]   ; Load 0.0 constant into a secondary SSE register
     
     ; double shortestDistance = 150000.0
-    movlpd xmm0, [const.shortest]
+    movlpd shortestDistance, [const.shortest]
+    
+    ; Handle permutation for initial array value
+    handle
 
-    ; Allocate (8 * (arrayLength + 1)) bytes for the array, initialize to NULL
+    ; Allocate (8 * (arrayLength)) bytes for the array, initialize to NULL
     ; int[] p = new int[array.length];
+    save_volatile
     invoke HeapCreate, 0, 0, 0
     mov [h_heap], rax
-    inc rdx
+    mov rdx, arrayLength
     shl rdx, 3
     invoke HeapAlloc, [h_heap], 0x8, rdx
-    mov [p], rax
+    mov p_array, rax
+    restore_volatile
     
-    ; for (int k = 0; k < limit; k++) {
-    ;     r += distances[array[k]][array[k + 1]];
-    ; }
-    mov rcx, [limit]            ; rcx = limit
-    test rcx, rcx
-    jz for_1_end                ; if rcx == 0 then skip
-    mov r8, [array]             ; r8 = array
-    mov r9, 1                   ; r9 = k + 1
-    for_1:
-        
-        mov rax, r8
-        mov rax, [rax]          ; rax = *(array + r8)
-        shl rax, 3
-        add rax, [distances]    ; rax = &distances[array[k]]
-        mov rax, [rax]          ; rax = distances[array[k]]
-        
-        mov rbx, r9
-        shl rbx, 3
-        add rbx, r8             ; rbx = &array[k + 1]
-        mov rbx, [rbx]          ; rbx = array[k + 1]
-        shl rbx, 3
-        add rbx, rax            ; rbx = distances[array[k]][array[k + 1]]
-        
-        addsd xmm1, [rbx]
-        ;;
-        ;movsd xmm0, xmm1
-        ;jmp cleanup
-        ;;
-    
-        add r8, 8
-        inc r9
-        loop for_1
-        for_1_end:
-        
-    ; r += distances[array[0]][array.length];
-    mov rax, [array]
-    mov rax, [rax]              ; rax = array[0]
-    shl rax, 3
-    add rax, [distances]        ; rax = distances + (array[0] * 8)
-    mov rax, [rax]              ; rax = *rax
-    mov rbx, [arrayLength]
-    ;dec rbx
-    shl rbx, 3                  ; rbx = (arrayLength - 1) * 8
-    add rbx, rax                ; rbx += rax
-    addsd xmm1, [rbx]
-    
-    ; r += distances[array[limit]][array.length];
-    mov rax, [limit]
-    shl rax, 3
-    add rax, [array]            ; rax = array + (limit * 8)
-    mov rax, [rax]              ; rax = *rax
-    shl rax, 3
-    add rax, [distances]        ; rax = distances + *rax
-    mov rax, [rax]              ; rax = *rax
-    mov rbx, [arrayLength]
-    shl rbx, 3                  ; rbx = (arrayLength - 1) * 8
-    add rbx, rax                ; rbx += rax
-    addsd xmm1, [rbx]
-    
-    ; if (r < shortestDistance) {
-    ;     shortestDistance = r;
-    ; }
-    if_1:
-        comisd xmm1, xmm0       ; if xmm1 < xmm0 then set CF
-        jnc if_1_end
-        movsd xmm0, xmm1        ; shortestDistance = r
-        if_1_end:
-    
-    ; int i = 1
-    mov r9, 1
-    
-    mov r10, [array]
-    mov r11, [p]
-    mov r12, [arrayLength]
-    mov r13, [limit]
-    mov r14, [distances]
-    
-    ; while (i < array.length) { ...
-    while_1:
-        cmp r9, r12
-        jae while_1_end
-        
-        ; if (p[i] < i) { ...
-        if_2:
-            mov rax, r9
-            shl rax, 3
-            add rax, r10        ; rax = &p[i]
-            mov r15, rax        ; r15 = rax -- save for later
-            mov rbx, [rax]      ; rbx = p[i]
-            cmp rbx, r9
-            jb else_2
-        
-            ; int j = i % 2 * p[i]
-            mov rax, r9
-            and rax, 1          ; rax = j = (i % 2)
-            mul rbx             ; rax = j = (i % 2) * p[i]
-            
-            ; xchg array[i], array[j]
-            shr rax, 3
-            add rax, r10        ; rax = &array[j]
-            mov rcx, [rax]      ; rcx = array[j]
-            
-            mov rbx, r9
-            shr rbx, 3
-            add rbx, r10        ; rbx = &array[i]
-            mov rdx, [rbx]      ; rdx = array[i]
-            
-            mov [rbx], rcx      ; array[i] = array[j]
-            mov [rax], rdx      ; array[j] = array[i]
-            
-            ; for (r = z = 0; z < limit;)
-            movsd xmm1, xmm7
-            xor r8, r8
-            for_2:
-                cmp r8, r13
-                jae for_2_end
-                
-                ; r += distances[array[z]][array[++z]]
-                mov rax, r8
-                shl rax, 3
-                add rax, r10    ; rax = &array[z]
-                mov rax, [rax]  ; rax = array[z]
-                
-                shl rax, 3
-                add rax, r14    ; rax = &distances[array[z]]
-                mov rax, [rax]  ; rax = distances[array[z]]
-                
-                inc r8
-                mov rbx, r8
-                shl rbx, 3
-                add rbx, r10    ; rbx = &array[++z]
-                mov rbx, [rbx]  ; rbx = array[++z]
-                
-                shl rbx, 3
-                add rbx, rax    ; rbx = &(distances[array[z]][array[++z]])
-                
-                addsd xmm1, [rbx]
-                                ; r += distances[array[z]][array[++z]]
-                                
-                jmp for_2
-                for_2_end:
-            
-            ; r += distances[array.length][array[0]]
-            mov rax, r12
-            shl rax, 3
-            add rax, r14        ; rax = &distances[array.length]
-            mov rax, [rax]      ; rax = distances[array.length]
-            
-            mov rbx, r10
-            mov rbx, [rbx]
-            shl rbx, 3
-            add rbx, rax        ; rbx = &(distances[array.length][array[0]])
-            
-            addsd xmm1, [rbx]   ; r += distances[array.length][array[0]]
-            
-            ; r += distances[array.length][array[limit]]
-            mov rbx, r13
-            shl rbx, 3
-            add rbx, r10        ; rbx = &array[limit]
-            mov rbx, [rbx]      ; rbx = array[limit]
-            
-            shl rbx, 3
-            add rbx, rax        ; rbx = &(distances[array.length][array[limit]])
-            
-            addsd xmm1, [rbx]   ; r += distances[array.length][array[limit]]
-            
-            ; if (r < shortestDistance) {
-            ;     shortestDistance = r
-            ; }
-            if_3:
-                comisd xmm1, xmm0       ; if xmm1 < xmm0 then set CF
-                jnc if_3_end
-                movsd xmm0, xmm1        ; shortestDistance = r
-                if_3_end:
-            
-            ; p[i]++
-            inc qword [r15]
-            
-            ; i = 1
-            mov r9, 1
-            
-            if_2_end:
-            jmp else_2_end
-        ; ... }
-        ; else { ...
-        else_2:
-            ; p[i] = 0
-            ; i++
-            xor rbx, rbx
-            mov [rax], rbx
-            inc r9
-            
-            else_2_end:
-        ; ... }
-        
-        jmp while_1
-        while_1_end:
-    ; ... }
+    ; ;
     
     ; Cleanup
     cleanup:
-    movsd xmm6, xmm0                    ; Save return value in nonvolatile SSE register
-    invoke HeapDestroy, [h_heap]        ; This will destroy xmm0 !!
-    movsd xmm0, xmm6
+    save_volatile
+    invoke HeapDestroy, [h_heap]
+    restore_volatile
     
     ; return shortestDistance
     ;
     ; If the function returns 0xFFFF instead, you are getting the integer value
     ; and not the (double) value as you should be. Check your code.
     mov rax, 0xFFFF
+    restore_nonvolatile
     ret
 endp
 
